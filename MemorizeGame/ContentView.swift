e6c38682077a3c57f296dff1da2d83ef95265af5
Martin Martinez <mtz.martin@proton.me>
2023-11-13 15:41:13 -0600

    I created a new View inside the same file and learn the fundamentals of SwiftUI, the core concepts

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -9,11 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        CardView()
+    }
+}
+
+
+struct CardView: View {
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 25)
+                .strokeBorder(Color.indigo,lineWidth: 4)
+                .overlay(
+                Text("🤓")
+                    .font(.largeTitle)
+                )
+            RoundedRectangle(cornerRadius: 25)
+                .strokeBorder(Color.indigo,lineWidth: 4)
+                .overlay(
+                Text("🤓")
+                    .font(.largeTitle)
+                )
+            RoundedRectangle(cornerRadius: 25)
+                .strokeBorder(Color.indigo,lineWidth: 4)
+                .overlay(
+                Text("🤓")
+                    .font(.largeTitle)
+                )
+            RoundedRectangle(cornerRadius: 25)
+                .strokeBorder(Color.indigo,lineWidth: 4)
+                .overlay(
+                Text("🤓")
+                    .font(.largeTitle)
+                )
         }
         .padding()
     }
