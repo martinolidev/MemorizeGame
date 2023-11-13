39d85b07350d6c4f9e6b182c8a5b5a8ebeadb853
Martin Martinez <mtz.martin@proton.me>
2023-11-13 14:52:25 -0600

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
